0733ca04ed5ee26734933a591755f95d92ebe763
cku3 <43300532+cku3@users.noreply.github.com>
2019-11-07 01:19:54 -0500

    all cans on shelf, spotlights, facts

---
+++ CS5650/Assets/clankClonk.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class clankClonk : MonoBehaviour
+{
+    GameObject can;
+    AudioSource canDrop;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        canDrop = can.GetComponent<AudioSource>();
+        //if the can hits the floor then:
+        if (!canDrop.isPlaying)
+        {
+            canDrop.Play();
+            Debug.Log("clink clank");
+        }
+    }
+}
