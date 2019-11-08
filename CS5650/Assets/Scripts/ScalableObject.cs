ee8a9bd5e5390da50ed9167a9bdf0065b8f0f585
Amanda Trang <at669@cornell.edu>
2019-11-07 22:59:15 -0500

    Initial scaling script

---
+++ CS5650/Assets/Scripts/ScalableObject.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace OculusSampleFramework
+{
+public class ScalableObject : MonoBehaviour
+{
+    public GameObject Top;
+    public GameObject Bottom;
+    private float origDist;
+    private Vector3 origScale;
+    private bool prevGrabbed;
+    private bool currentlyGrabbed;
+    private Vector3 tmpTopPos;
+    private Vector3 tmpBotPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        origDist = Vector3.Distance(Top.transform.position, Bottom.transform.position);
+        origScale = transform.localScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Top.GetComponent<OVRGrabbable>().isGrabbed && Bottom.GetComponent<OVRGrabbable>().isGrabbed){
+            currentlyGrabbed = true;
+            float newDist = Vector3.Distance(Top.transform.position, Bottom.transform.position);
+            if (newDist - origDist > 0.0001f){ // check if stretched
+                float ratio = newDist / origDist;
+                Vector3 newScale = ratio * origScale;
+                transform.localScale = newScale;
+
+                // transform.position = (0.5f * (Top.transform.position + Bottom.transform.position));
+
+                origScale = newScale;
+                origDist = Vector3.Distance(Top.transform.position, Bottom.transform.position);
+                // tmpTopPos = Top.transform.position;
+                // tmpBotPos = Bottom.transform.position;
+            }
+        }
+        else{
+            currentlyGrabbed = false;
+        }
+        if (!currentlyGrabbed && prevGrabbed){ // just let go of both
+            // Top.transform.position = tmpTopPos;
+            // Bottom.transform.position = tmpBotPos;
+            // origDist = Vector3.Distance(Top.transform.position, Bottom.transform.position);
+        }
+        prevGrabbed = currentlyGrabbed;
+        
+    }
+}
+}
