3518fd1b432071ef7ff8f90a6d671b3406bd0d9c
Amanda Trang <at669@cornell.edu>
2019-12-02 14:31:46 -0500

    Spin/scale shows up next to ONE statue

---
+++ CS5650/Assets/Scripts/ArtworkCast.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Diagnostics;
+using UnityEngine.UI;
+using Debug = UnityEngine.Debug;
+using UnityEngine.EventSystems;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+// Show off all the Debug UI components.
+public class ArtworkCast : MonoBehaviour
+{
+    public string PaintingTitle;
+    public string Artist;
+    public string Year;
+    private LocomotionController lc;
+    private bool inMenu = false;
+    public int artID;
+    private LocomotionTeleport TeleportController
+    {
+        get
+        {
+            return lc.GetComponent<LocomotionTeleport>(); 
+        }
+    }
+
+	// protected override void Start() {
+    void Start() {
+        lc = FindObjectOfType<LocomotionController>();
+
+        DebugUIBuilder.instance.AddLabel(PaintingTitle, artID);
+        DebugUIBuilder.instance.AddLabel(Artist, artID);
+        DebugUIBuilder.instance.AddLabel(Year, artID);
+        DebugUIBuilder.instance.AddButton("Close", Close, artID);
+        DebugUIBuilder.instance.AddButton("Enter", Enter, artID);
+
+        DebugUIBuilder.instance.Show();
+
+        EventSystem eventSystem = FindObjectOfType<EventSystem>();
+	}
+
+    void Enter(){
+        SceneManager.LoadScene(PaintingTitle);
+    }
+
+    void Close()
+    {
+        if (inMenu) DebugUIBuilder.instance.Hide();
+        inMenu = false;
+    }
+
+    void Update()
+    {
+        // if(OVRInput.GetDown(OVRInput.Button.Two) || OVRInput.GetDown(OVRInput.Button.Start))
+        // {
+        //     if (inMenu) DebugUIBuilder.instance.Hide();
+        //     else DebugUIBuilder.instance.Show();
+        //     inMenu = !inMenu;
+        // }
+
+        // if (OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger) > 0.1f){
+        //     RaycastHit rayHit; 
+        //     Ray ray = new Ray(m_gripTransform.position, m_gripTransform.forward);
+        //     if (Physics.Raycast(ray, out rayHit, Mathf.Infinity)) {
+        //         Interact(rayHit);
+        //     }
+        // }
+    }
+
+    private void Interact(RaycastHit hit){
+        if (hit.collider.name.StartsWith("Paint")){
+            if (inMenu) DebugUIBuilder.instance.Hide();
+            else DebugUIBuilder.instance.Show();
+            inMenu = !inMenu;
+        }
+        else{
+            if (inMenu) DebugUIBuilder.instance.Hide();
+            inMenu = false;
+        }
+    }
+}
--- CS5650/Assets/Scripts/SculptSlider.cs
+++ CS5650/Assets/Scripts/SculptSlider.cs
@@ -9,11 +9,15 @@ using UnityEngine.Events;
 // Show off all the Debug UI components.
 public class SculptSlider : MonoBehaviour
 {
-
+    public string Title;
+    public string Artist;
+    public string Year;
     private LocomotionController lc;
     private bool inMenu = false;
     private Text sliderText;
     private Text rotText;
+    // LaserPointer lp;
+    // public LaserPointer.LaserBeamBehavior laserBeamBehavior;
     private LocomotionTeleport TeleportController
     {
         get
@@ -21,32 +25,41 @@ public class SculptSlider : MonoBehaviour
             return lc.GetComponent<LocomotionTeleport>(); 
         }
     }
-    // bool inMenu;
     public Vector3 origScale;
-    public int sculptID = 0;
+    public int sculptID;
     private float prevRot = 0;
 
-	void Start ()
-    {
+    // void Awake(){
+    //     lp = FindObjectOfType<LaserPointer>();
+    //     lp.laserBeamBehavior = laserBeamBehavior;
+    //     GetComponent<OVRRaycaster>().pointer = lp.gameObject;
+    //     lp.gameObject.SetActive(true);
+    // }
+
+	// protected override void Start() {
+    void Start() {
         lc = FindObjectOfType<LocomotionController>();
+        DebugUIBuilder.instance.AddLabel(Title, sculptID);
+        DebugUIBuilder.instance.AddLabel(Artist, sculptID);
+        DebugUIBuilder.instance.AddLabel(Year, sculptID);
+
         var sliderPrefab = DebugUIBuilder.instance.AddSlider("Scale", 1f, 2.0f, SliderPressed, false, sculptID);
         var sliderRot = DebugUIBuilder.instance.AddSlider("Rotation", 0f, 360f, SliderRotate, true, sculptID);
         var textElementsInSlider = sliderPrefab.GetComponentsInChildren<Text>();
         var textElementsInRotSlider = sliderRot.GetComponentsInChildren<Text>();
+        textElementsInSlider[0].text = "Scale:";
+        textElementsInRotSlider[0].text = "Rotation:";
         sliderText = textElementsInSlider[1];
         sliderText.text = sliderRot.GetComponentInChildren<Slider>().value.ToString();
         rotText = textElementsInRotSlider[1];
         rotText.text = sliderRot.GetComponentInChildren<Slider>().value.ToString();
-        DebugUIBuilder.instance.Show();
-        inMenu = true;
+        DebugUIBuilder.instance.SculptShow(sculptID);
+        DebugUIBuilder.instance.Hide();
+        inMenu = false;
 
         origScale = transform.localScale;
 
         EventSystem eventSystem = FindObjectOfType<EventSystem>();
-        if (eventSystem == null)
-        {
-            Debug.LogError("Need EventSystem");
-        }
 	}
 
     public void SliderPressed(float f)
@@ -67,8 +80,29 @@ public class SculptSlider : MonoBehaviour
         if(OVRInput.GetDown(OVRInput.Button.Two) || OVRInput.GetDown(OVRInput.Button.Start))
         {
             if (inMenu) DebugUIBuilder.instance.Hide();
-            else DebugUIBuilder.instance.Show();
+            else DebugUIBuilder.instance.SculptShow(sculptID);
             inMenu = !inMenu;
+        }
+
+        if (OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger) > 0.1f){
+            RaycastHit rayHit; 
+            // Ray ray = new Ray(m_gripTransform.position, m_gripTransform.forward);
+            Ray ray = new Ray(GetComponent<OVRRaycaster>().pointer.transform.position, GetComponent<OVRRaycaster>().pointer.transform.forward);
+            if (Physics.Raycast(ray, out rayHit, Mathf.Infinity)) {
+                Interact(rayHit);
+            }
+        }
+    }
+
+    private void Interact(RaycastHit hit){
+        if (hit.collider.name.StartsWith("Sculpt")){
+            if (inMenu) DebugUIBuilder.instance.Hide();
+            else DebugUIBuilder.instance.SculptShow(sculptID);
+            inMenu = !inMenu;
+        }
+        else{
+            if (inMenu) DebugUIBuilder.instance.Hide();
+            inMenu = false;
         }
     }
 }
