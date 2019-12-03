82348325fb56bbe3cc01fdbbb6641e6421bfb6e5
cku3 <43300532+cku3@users.noreply.github.com>
2019-12-02 23:55:24 -0500

    some fun interaction

---
+++ CS5650/Assets/Scripts/armyOfAnts.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Oculus.Avatar;
+
+public class armyOfAnts : MonoBehaviour
+{
+    //if the clock is grabbed, instantiate a bunch of smolAnts
+    OVRGrabbable grabState;
+    public GameObject smolAnt;
+    //public GameObject ClockAnts;
+    public GameObject Platform2;
+    public GameObject giAnt;
+    bool activated;
+    // Start is called before the first frame update
+    void Start()
+    {
+        grabState = GetComponent<OVRGrabbable>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (grabState.isGrabbed == true)
+        {
+            Instantiate(smolAnt, transform.position, Quaternion.identity);
+        }
+
+        if (grabState.isGrabbed == true && (OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger) > 0.5f))
+        {
+            activated = true;
+        }
+
+        if (activated)
+        {
+            Instantiate(giAnt, Platform2.transform.position, Quaternion.identity);
+            activated = false;
+        }
+    }
+}
---
+++ CS5650/Assets/Scripts/flyFly.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flyFly : MonoBehaviour
+{
+    //public GameObject fly;
+    float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //start position
+        transform.position = new Vector3(470.8f, 45.9f, 438.4f);
+        speed = 0.4f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //lerp to 499.53, 6.73, 409.7
+        Vector3 finalPosition = new Vector3(499.53f, 6.73f, 409.7f);
+        transform.position = Vector3.Lerp(transform.position, finalPosition, speed * Time.deltaTime);
+    }
+}
